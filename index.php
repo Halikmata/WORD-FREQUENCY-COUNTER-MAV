ff4b8f6c327daf076732010f72f9d088341deae5
Jason Maverick Diokno Serencio <202180383@psu.palawan.edu.ph>
2023-10-03 22:33:02 +0800

    Revert "fixed more"
    This reverts commit e452eb2d48ad6ce106f3b2ed59f49e517b668ea5.

--- index.php
+++ index.php
@@ -4,19 +4,6 @@
     <meta charset="UTF-8">
     <title>Word Frequency Counter</title>
     <link rel="stylesheet" type="text/css" href="styles.css">
-    <style>
-        /* Center the form container */
-        form {
-            background-color: #fff;
-            border: 1px solid #ccc;
-            padding: 20px;
-            max-width: 600px;
-            margin: 0 auto;
-            border-radius: 5px;
-            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
-            text-align: left; /* Align the form content to the left */
-        }
-    </style>
 </head>
 
 <?php
